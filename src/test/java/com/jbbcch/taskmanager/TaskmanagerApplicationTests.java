39b2bf2490ccb04c9c12ab95e0d1a62d63100f0f
Jbbcch <ceyhunmehdyiv@gmail.com>
2025-10-01 15:16:16 +0400

    Initial commit.

---
+++ src/test/java/com/jbbcch/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbbcch.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
