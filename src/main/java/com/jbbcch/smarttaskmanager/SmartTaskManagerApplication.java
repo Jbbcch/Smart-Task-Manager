fd9e624630f0df77ebfec7bc7f3f2239874faedd
Jbbcch <ceyhunmehdyiv@gmail.com>
2025-10-03 17:21:35 +0400

    Integration with spring modulith: - Added modulith dependencies - Changed project structure to a modular one - Renamed some files and packages

--- src/main/java/com/jbbcch/smarttaskmanager/SmartTaskManagerApplication.java
+++ src/main/java/com/jbbcch/smarttaskmanager/SmartTaskManagerApplication.java
@@ -1,13 +1,13 @@
-package com.jbbcch.taskmanager;
+package com.jbbcch.smarttaskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskManagerApplication {
+public class SmartTaskManagerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskManagerApplication.class, args);
+		SpringApplication.run(SmartTaskManagerApplication.class, args);
 	}
 
 }
