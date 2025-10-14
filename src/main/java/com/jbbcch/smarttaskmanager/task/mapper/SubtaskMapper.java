361b8ee9c5a18f747733f71d9515ae1466603c63
Jbbcch <ceyhunmehdyiv@gmail.com>
2025-10-15 02:09:03 +0400

    Added subtask mapper. - I forgot to add it in the previous commit :P

---
+++ src/main/java/com/jbbcch/smarttaskmanager/task/mapper/SubtaskMapper.java
@@ -0,0 +1,12 @@
+package com.jbbcch.smarttaskmanager.task.mapper;
+
+import com.jbbcch.smarttaskmanager.task.external.dto.SubtaskRequest;
+import com.jbbcch.smarttaskmanager.task.external.dto.SubtaskResponse;
+import com.jbbcch.smarttaskmanager.task.model.entity.Subtask;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface SubtaskMapper {
+    SubtaskResponse subtaskToSubtaskResponse(Subtask subtask);
+    Subtask subtaskRequestToSubtask(SubtaskRequest subtaskRequest);
+}
