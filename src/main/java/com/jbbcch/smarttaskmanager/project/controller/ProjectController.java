f36084a1ec30b7629d1c02761ed6bca2e6802a7b
Jbbcch <ceyhunmehdyiv@gmail.com>
2025-10-22 00:15:55 +0400

    Changed request mapping /project to /projects. - Very meaningful commit, I know.

--- src/main/java/com/jbbcch/smarttaskmanager/project/controller/ProjectController.java
+++ src/main/java/com/jbbcch/smarttaskmanager/project/controller/ProjectController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/project")
+@RequestMapping("/projects")
 @RequiredArgsConstructor
 public class ProjectController {
     private final ProjectInternalAPI projectInternalAPI;
