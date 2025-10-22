9428aa1f36c8c65183c6755d2907f69bb804361f
Jbbcch <ceyhunmehdyiv@gmail.com>
2025-10-22 11:15:05 +0400

    Wrote user entity.

---
+++ src/main/java/com/jbbcch/smarttaskmanager/user/model/entity/User.java
@@ -0,0 +1,59 @@
+package com.jbbcch.smarttaskmanager.user.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users", schema = "organization_management")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@DynamicInsert
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    @Column(nullable = false)
+    String username;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    String email;
+
+    String firstName;
+
+    String lastName;
+
+    @Column(nullable = false, updatable = false)
+    LocalDateTime createdAt;
+
+    LocalDateTime updatedAt;
+
+    @Column(nullable = false, updatable = false)
+    UUID createdBy;
+
+    UUID updatedBy;
+
+    // defaults to false on db level
+    Boolean isDeleted;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
