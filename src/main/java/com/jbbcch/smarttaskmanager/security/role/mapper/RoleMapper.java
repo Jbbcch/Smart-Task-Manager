cff0fedb34887d8c3f7475ab64b6ecc93eeaa757
Jbbcch <ceyhunmehdyiv@gmail.com>
2025-11-17 23:28:41 +0400

    Updated mapper annotation for the role mapper.

--- src/main/java/com/jbbcch/smarttaskmanager/security/role/mapper/RoleMapper.java
+++ src/main/java/com/jbbcch/smarttaskmanager/security/role/mapper/RoleMapper.java
@@ -4,8 +4,9 @@ import com.jbbcch.smarttaskmanager.security.role.dto.RoleRequest;
 import com.jbbcch.smarttaskmanager.security.role.dto.RoleResponse;
 import com.jbbcch.smarttaskmanager.security.role.model.entity.Role;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RoleMapper {
     RoleResponse roleToRoleResponse(Role role);
     Role roleRequestToRole(RoleRequest roleRequest);
